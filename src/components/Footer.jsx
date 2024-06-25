3d9072cb25c729b7d8c2c7bba36e9aefe2ee6f32
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-06-25 12:37:59 -0300

    change page title and add links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return <div className="footer">
-    <p>Made by Facundo | <a href="">Github</a> | <a href="">The Odin Project</a></p>
+    <p>Made by Facundo | <a href="https://github.com/Uruwhy1/odin-memoryCard">Github</a> | <a href="https://www.theodinproject.com/lessons/node-path-react-new-memory-card">The Odin Project</a></p>
   </div>;
 }
